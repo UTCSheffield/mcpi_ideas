7bc0e01f0d82f7403d5a561c5fc31ae908e638ea
Mr Eggleton <meggleton@utcsheffield.org.uk>
2017-04-06 08:39:13 +0000

    Code to get the players position

---
+++ 1_Get_Started/1_Get_Started.py
@@ -0,0 +1,20 @@
+from mcpi import minecraft
+from mcpi import block
+
+# Store the connection to Minecraft in a variable called mc
+mc = minecraft.Minecraft.create()
+
+# Store the position player is standing in a variable called pos 
+pos = mc.player.getTilePos() 
+
+# Store a string which contains the x,y,z of your position
+message = "You are at x="+str(pos.x)+", y="+str(pos.y)+", z="+str(pos.z)
+
+# Print the message in the python screen
+print(message)
+
+# Send the message to the Minecraft chat
+mc.postToChat(message)
+
+# A quicker way to print the position
+message2 = "You are at "+str(pos)
