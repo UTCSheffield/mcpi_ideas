a1868a8f851e45b47e2b930924090a16207094a6
Mr Eggleton <meggleton@utcsheffield.org.uk>
2017-06-27 11:47:57 +0100

    Create teleport.py

---
+++ teleport.py
@@ -0,0 +1,15 @@
+from mcpi import minecraft
+from mcpi import block
+
+# Store the connection to Minecraft in a variable called mc
+mc = minecraft.Minecraft.create("127.0.0.1")
+
+# Store the position player is standing in a variable called pos 
+pos = mc.player.getTilePos() 
+
+newpos = pos
+newpos.y += 20
+mc.player.setTilePos(newpos)
+
+
+#mc.player.setTilePos(pos.x, pos.y+20, pos.z)
