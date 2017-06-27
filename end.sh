7f688431b5b7e60b9350264b9365113e44e91514
Mr Eggleton <meggleton@utcsheffield.org.uk>
2017-06-27 17:16:18 +0100

    The work of Mr Eggleton

--- end.sh
+++ end.sh
@@ -4,7 +4,8 @@ StudentName=""
 Branches=`git branch`
 #while [!"$StudentName"]
 #do
-    PossName=`zenity --entry --title "Your Name" --text "Please enter your name so we can store your work"`
+    StudentName=`zenity --entry --title "Your Name" --text "Please enter your name so we can store your work"`
+#    PossName=`zenity --entry --title "Your Name" --text "Please enter your name so we can store your work"`
  #   if [[$Branches != *"$PossName"*]]; then
  #       StudentName="$PossName"
  #   fi
