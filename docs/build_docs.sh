ca92b3963055ecae8f0b6e130e4c9c1abf55dd58
mr_eggleton <meggleton@utcsheffield.org.uk>
2017-07-05 04:27:12 +0000

    Fixes #14  put the workshop end bit into the learning materials

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# For all the numbered mardown files
+# For all the numbered markdown files
 for f in [0-9]*.md; 
 do 
     #Render to a static web page (with all pictures embedded)
@@ -10,3 +10,18 @@ do
     #Render to PDF
     pandoc "$f" -s -f markdown_github --highlight-style=kate -V geometry:margin=0.5in -o "pdf/${f%.md}.pdf"; 
 done
+
+# TODO : New Page Per file
+# TODO : Logo in header?
+# TODO : Add the URL in footer or in additional end section
+pandoc -s -f markdown_github --highlight-style=kate -V geometry:margin=0.5in -o "pdf/home.pdf" [0-9]*.md
+pandoc -s -f markdown_github --highlight-style=kate -V geometry:margin=0.5in -o "pdf/workshop.pdf" [0-9]*.md workshop_end.md
+
+# TODO : Logo in background?
+pandoc -s -f markdown_github --highlight-style=kate --self-contained -t slidy -o "slideshow/home.html" [0-9]*.md
+pandoc -s -f markdown_github --highlight-style=kate --self-contained -t slidy -o "slideshow/workshop.html" [0-9]*.md workshop_end.md
+
+pandoc -s -f markdown_github --highlight-style=kate --self-contained -t html5 -o "html/home.html"  [0-9]*.md
+pandoc -s -f markdown_github --highlight-style=kate --self-contained -t html5 -o "html/workshop.html"  [0-9]*.md workshop_end.md
+    
+
---
+++ docs/workshop_end.md
@@ -0,0 +1,19 @@
+# We are out of time
+
+To help us get ready fo the next session could you please
+
+1. Save your work
+2. Close the Python editor (IDLE) and any Python windows
+2. Close Minecraft
+3. In the "mcpi_ideas" folder, double click on "end_session.sh"
+4. Click the "Execute" button
+5. Enter your name (without spaces)
+6. Hit enter (this will create a github branch for your work and save it there)
+
+# Thank you
+
+![UTC Logo](images/UTC_Logo.png)
+
+This session and all the supporting code is available to download at our GitHub.
+
+[https://github.com/UTC-Sheffield/mcpi_ideas](https://github.com/UTC-Sheffield/mcpi_ideas)
