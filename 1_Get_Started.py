95a31c0db2331e6bd98a3a364544122ec4e88d62
Unknown4 <Unknown4@notregistered.com>
2017-07-06 12:51:09 +0000

    The work of JackUnderwood

--- 1_Get_Started.py
+++ 1_Get_Started.py
@@ -4,18 +4,10 @@ from mcpi import block
 # Store the connection to Minecraft in a variable called mc
 mc = minecraft.Minecraft.create()
 
-# Store the position player is standing in a variable called pos 
-pos = mc.player.getTilePos() 
 
-# Store a string which contains the x,y,z of your position
-message = "You are at x="+str(pos.x)+", y="+str(pos.y)+", z="+str(pos.z)
-
-# Print the message in the python screen
-print(message)
-
-# Send the message to the Minecraft chat
-mc.postToChat(message)
-
-# A quicker way to print the position
-message2 = "You are at "+str(pos)
-print(message2)
+while True:
+    pos = mc.player.getTilePos()
+    num = (pos.x+pos.y+pos.z) % 16
+    tempy = pos.y - 1
+    pos.y = tempy
+    mc.setBlock(pos, block.WOOL.id, num)
--- 2_Teleporting.py
+++ 2_Teleporting.py
@@ -2,12 +2,12 @@ from mcpi import minecraft
 from mcpi import block
 
 # Store the connection to Minecraft in a variable called mc
-ip_addr = "127.0.0.1"
+ip_addr = "192.168.1.30"
 mc = minecraft.Minecraft.create(ip_addr)
 
 # Store the position player is standing in a variable called pos 
 pos = mc.player.getTilePos() 
 
 newpos = pos
-newpos.y += 20
+newpos.y += 4000
 mc.player.setTilePos(newpos)
